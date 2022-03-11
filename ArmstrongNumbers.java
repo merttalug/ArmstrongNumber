f04c409cea722510b9b9d89bd921b04d8f03dd7e
Mert Talug <merttalug14@gmail.com>
2022-03-11 16:38:30 +0300

    Create ArmstrongNumbers.java

---
+++ ArmstrongNumbers.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class ArmstrongNumbers {
+    public static void main(String[] args) {
+        int number, digitnumber = 0, digitvalue = 1, res = 0, digitpow, tempnumber = 0;
+        boolean asknumber = false;
+        Scanner input = new Scanner(System.in);
+
+        do {
+            System.out.println("Please enter a number: ");
+            number = input.nextInt();
+            if (number > 0 && number <= 999) {
+                asknumber = true;
+            }
+        }
+        while (!asknumber);
+
+        while (tempnumber != 0) {
+            tempnumber /= 10;
+            digitnumber++;
+        }
+
+        tempnumber = number;
+
+        while (tempnumber != 0) {
+            digitvalue = tempnumber % 10;
+            digitpow = 1;
+            for (int i = 1; i <= digitnumber; i++) {
+                digitpow *= digitvalue;
+            }
+            res += digitpow;
+            tempnumber /= 10;
+        }
+
+
+        if (res == number) {
+            System.out.println(number + " is an Armstrong Number.");
+        } else {
+            System.out.println(number + " is not an Armstrong Number.");
+
+
+        }
+
+    }
+}
+
